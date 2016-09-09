512cf226963db937ffb9d5eabf0250adbd1a1699
dez <lfagames@gmail.com>
2016-09-09 12:26:08 -0400

    adding minor bug fixes before 1.1

--- internal/hypercubeCamera.cs
+++ internal/hypercubeCamera.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
     [ExecuteInEditMode]
     public class hypercubeCamera : MonoBehaviour
     {
-         public const float version = 1.01f;
+         public const float version = 1.1f;
 
         public enum softSliceMode
         {
