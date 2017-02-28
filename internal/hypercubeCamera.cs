94ebe60121ddc88764887618e3bb564905958769
Dez <dez@lfagames.com>
2017-02-28 18:19:39 -0500

    changed ver num 2.11

--- internal/hypercubeCamera.cs
+++ internal/hypercubeCamera.cs
@@ -28,7 +28,7 @@ using System.Collections.Generic;
     [ExecuteInEditMode]
     public class hypercubeCamera : MonoBehaviour
     {
-         public const float version = 2.1f;
+         public const float version = 2.11f;
 
          //a static pointer to the last activated hypercubeCameraZ
          public static hypercubeCamera mainCam = null;  
