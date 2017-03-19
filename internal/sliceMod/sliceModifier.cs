3e01b57d0964f439ee3fe90440e0ebd53d3ea0e7
Dez <dez@lfagames.com>
2017-03-19 07:52:39 -0400

    fixed bug on sliceModifier, tweaks to caster
    caster now will prompt with volume prefs.
    preview camera now defaults to only see things in UI layer

--- internal/sliceMod/sliceModifier.cs
+++ internal/sliceMod/sliceModifier.cs
@@ -14,7 +14,7 @@ namespace hypercube
     [System.Serializable]
     public class sliceModifier
     {
-        [Tooltip("What slice should this slice modifier apply to.\nDifferent devices have different amounts of slices, so use this to let the tools choose the appropriate slice between front (0) and back(1).")]
+        [Tooltip("The slice that this slice modifier should apply to.\nDifferent models of Volume have different amounts of slices. This is used to select the appropriate slice between front (0) and back (1).\n\nNOTE: to change this value from code in realtime, use setDepth(d) on the sliceModifier.")]
         [Range(0f, 1f)]
         public float depth; //0-1 front to back
         private float _lastDepth = -1; //used to detect change in value, while still allowing depth editability in inspector (don't use get/set)
@@ -25,12 +25,12 @@ namespace hypercube
 
         /// <summary>
         /// Use this call when dynamically setting depth from code.
-        /// This way, it will can get updated properly.
+        /// This way, it will get updated properly.
         /// </summary>
         /// <param name="d">What depth value to set this modifier to. 0 = front, 1 = back</param>
         public void setDepth(float d)
         {
-            d = Mathf.Clamp(d, 0f, 1f);
+            depth = Mathf.Clamp(d, 0f, 1f);      
 
             if (!hypercubeCamera.mainCam)
                 updateSliceModifiers(null);
@@ -74,21 +74,6 @@ namespace hypercube
                 allModifiers[s] = m;
             }
         }
-
-
-
-        //public static sliceModifier getSliceMod(int sliceNum, int sliceCount, sliceModifier[] mods) //TODO this iterates every slice on every frame, check if optimization is in order.
-        //{
-        //    if (sliceCount < 1 || mods == null)
-        //        return null;
-
-        //    foreach (sliceModifier m in mods)
-        //    {
-        //        if (m.getSlice(sliceCount) == sliceNum)
-        //            return m;
-        //    }
-        //    return null;
-        //}
 
         int slice = -1;
         public int updateSlice()
