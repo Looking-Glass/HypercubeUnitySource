69bc626205c8f840ae4fc539938291472e2840d3
Dez <dez@lookingglassfactory.com>
2017-04-13 15:10:05 -0400

    added tools for printing out detailed debug data

---
+++ internal/Editor/buildDateMarker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//using UnityEditor;
+//using UnityEditor.Build;
+
+//class buildDateMarker : IPreprocessBuild
+//{
+//    public int CallbackOrder { get { return 0; } }
+//    public void OnPreprocessBuild(BuildTarget target, string path)
+//    {
+//        Debug.Log("Build target: " + target + " at path " + path);
+//    }
+//}
